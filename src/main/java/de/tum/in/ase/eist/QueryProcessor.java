b6f4ff837b4a3f3599151dd36c50fcb3a7d8c38b
livi <liv.maertens@tum.de>
2022-07-14 13:08:28 +0200

    largest number

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,8 +13,18 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "Liv";
-        } else if (query.contains("what is")){ // TODO extend the programm here
-            return "" + (Integer.parseInt(query.split(" ")[3]) + Integer.parseInt(query.split(" ")[5])) ;
+        } else if (query.contains("what is")) { // TODO extend the programm here
+            return "" + (Integer.parseInt(query.split(" ")[3]) + Integer.parseInt(query.split(" ")[5]));
+        }else if(query.contains("largest")){
+            String[] a = query.split(":")[2].split(",");
+            int max = 0;
+            for (int i = 0; i < a.length(); i++) {
+                int b = Integer.parseInt(a[i]);
+                if( b >= max) {
+                    max = b;
+                }
+            }
+            return "" + max;
         }else{
             return "";
         }
